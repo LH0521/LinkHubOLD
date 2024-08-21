e01a97c860aec18daa500f44c9a20c094715766e
Lucas Hansen <lucashansen0521@gmail.com>
2024-08-21 01:40:08 -0400

    .

--- index.js
+++ index.js
@@ -420,38 +420,36 @@ document.addEventListener('DOMContentLoaded', () => {
                                 </div>
                             </div>
                             <hr class="my-7">
+                            <!--
+                                <div class="row justify-content-between align-items-center">
+                                    <div class="col-4">
+                                        <span class="d-block h6 text-heading mb-0" id="link-rating">5.0</span>
+                                        <span class="d-block text-sm text-muted">Rating</span>
+                                    </div>
+                                    <div class="col-4">
+                                        <span class="d-block h6 text-heading mb-0" id="link-saves">0</span>
+                                        <span class="d-block text-sm text-muted">Saves</span>
+                                    </div>
+                                    <div class="col-4">
+                                        <span class="d-block h6 text-heading mb-0" id="link-opens">0</span>
+                                        <span class="d-block text-sm text-muted">Opens</span>
+                                    </div>
+                                </div>
+                            -->
                             <div class="row justify-content-between align-items-center">
                                 <div class="col-4">
-                                    <span class="d-block h6 text-heading mb-0" id="link-rating">5.0</span>
-                                    <span class="d-block text-sm text-muted">Rating</span>
+                                    <span class="d-block h6 text-heading mb-0">${link.search.sexuality}</span>
+                                    <span class="d-block text-sm text-muted">Sexuality</span>
                                 </div>
                                 <div class="col-4">
-                                    <span class="d-block h6 text-heading mb-0" id="link-saves">0</span>
-                                    <span class="d-block text-sm text-muted">Saves</span>
+                                    <span class="d-block h6 text-heading mb-0">${link.search.body}</span>
+                                    <span class="d-block text-sm text-muted">Body</span>
                                 </div>
                                 <div class="col-4">
-                                    <span class="d-block h6 text-heading mb-0" id="link-opens">0</span>
-                                    <span class="d-block text-sm text-muted">Opens</span>
+                                    <span class="d-block h6 text-heading mb-0">${link.search.race}</span>
+                                    <span class="d-block text-sm text-muted">Race</span>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card mb-3">
-                <div class="card-body">
-                    <div class="row justify-content-between align-items-center">
-                        <div class="col-4">
-                            <span class="d-block h6 text-heading mb-0">${link.search.sexuality}</span>
-                            <span class="d-block text-sm text-muted">Sexuality</span>
-                        </div>
-                        <div class="col-4">
-                            <span class="d-block h6 text-heading mb-0">${link.search.body}</span>
-                            <span class="d-block text-sm text-muted">Body</span>
-                        </div>
-                        <div class="col-4">
-                            <span class="d-block h6 text-heading mb-0">${link.search.race}</span>
-                            <span class="d-block text-sm text-muted">Race</span>
                         </div>
                     </div>
                 </div>
@@ -465,50 +463,52 @@ document.addEventListener('DOMContentLoaded', () => {
                     <span class="d-block text-sm text-muted">${link.search.tags.join(', ')}</span>
                 </div>
             </div>
-            <div class="card">
-                <div class="card-body">
-                    <div class="d-flex align-items-center">
-                        <div>
-                            <span class="d-block h6 text-heading mb-0">Give ${link.name} a rating</span>
-                            <span class="d-block text-sm text-muted">0 Votes | Your Vote <b>5</b></span>
+            <!--
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <div>
+                                <span class="d-block h6 text-heading mb-0">Give ${link.name} a rating</span>
+                                <span class="d-block text-sm text-muted">0 Votes | Your Vote <b>5</b></span>
+                            </div>
+                            <button class="btn btn-sm btn-neutral ms-auto">Information</button>
                         </div>
-                        <button class="btn btn-sm btn-neutral ms-auto">Information</button>
-                    </div>
-                    <hr class="my-3">
-                    <div class="rating">
-                        <input type="radio" id="star5" name="rate" value="5" />
-                        <label title="Excellent!" for="star5">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
-                            </svg>
-                        </label>
-                        <input value="4" name="rate" id="star4" type="radio" />
-                        <label title="Great!" for="star4">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
-                            </svg>
-                        </label>
-                        <input value="3" name="rate" id="star3" type="radio" />
-                        <label title="Good" for="star3">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
-                            </svg>
-                        </label>
-                        <input value="2" name="rate" id="star2" type="radio" />
-                        <label title="Okay" for="star2">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
-                            </svg>
-                        </label>
-                        <input value="1" name="rate" id="star1" type="radio" />
-                        <label title="Bad" for="star1">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
-                            </svg>
-                        </label>
+                        <hr class="my-3">
+                        <div class="rating">
+                            <input type="radio" id="star5" name="rate" value="5" />
+                            <label title="Excellent!" for="star5">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
+                                </svg>
+                            </label>
+                            <input value="4" name="rate" id="star4" type="radio" />
+                            <label title="Great!" for="star4">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
+                                </svg>
+                            </label>
+                            <input value="3" name="rate" id="star3" type="radio" />
+                            <label title="Good" for="star3">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
+                                </svg>
+                            </label>
+                            <input value="2" name="rate" id="star2" type="radio" />
+                            <label title="Okay" for="star2">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
+                                </svg>
+                            </label>
+                            <input value="1" name="rate" id="star1" type="radio" />
+                            <label title="Bad" for="star1">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
+                                </svg>
+                            </label>
+                        </div>
                     </div>
                 </div>
-            </div>
+            -->
         `;
 
         const linkCanvas = new bootstrap.Offcanvas(document.getElementById('link_canvas'));
