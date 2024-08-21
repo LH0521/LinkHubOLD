ee234d6e189aa43f867b3e6f61ac74f8d31b327f
Lucas Hansen <lucashansen0521@gmail.com>
2024-08-21 01:30:49 -0400

    .

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ document.addEventListener('DOMContentLoaded', () => {
             document.querySelectorAll('.user-profile-pic').forEach(element => {
                 element.src = user.photoURL || 'default-profile-pic.png';
             });
-    
+
             displaySavedLinks();
         } else {
             document.querySelectorAll('.user-name').forEach(element => {
@@ -504,8 +504,7 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
         }
 
-        filteredlinks.forEach(({ originalIndex, ...link }) => {
-            const linkPrefix = link.flags.source === 'reddit' ? 'u/' : '@';
+        filteredlinks.forEach(({ link, originalIndex }) => {
             const linkElement = document.createElement('div');
             linkElement.classList.add('col-lg-4', 'col-sm-6');
 
@@ -518,12 +517,16 @@ document.addEventListener('DOMContentLoaded', () => {
                             </div>
                             <div class="flex-1">
                                 <a href="#" class="d-block font-semibold text-sm text-heading text-primary-hover">${link.name}</a>
-                                <div class="text-xs text-muted line-clamp-1">${linkPrefix}${link.link}</div>
+                                <div class="text-xs text-muted line-clamp-1">@${link.link}</div>
                             </div>
                             <div class="text-end">
-                                <button type="button" class="btn btn-sm btn-neutral rounded-pill view-button" data-index="${originalIndex}">
+                                <button type="button" class="btn btn-sm btn-neutral rounded-pill view-button" data-link-id="${link.link}">
                                     <i class="bi bi-folder2-open me-1"></i>
                                     <span>View</span>
+                                </button>
+                                <button type="button" class="btn btn-sm btn-neutral rounded-pill save-button" data-link-id="${link.link}">
+                                    <i class="bi bi-save2 me-1"></i>
+                                    <span>Save</span>
                                 </button>
                             </div>
                         </div>
@@ -536,25 +539,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
         document.querySelectorAll('.view-button').forEach(button => {
             button.addEventListener('click', function () {
-                const index = this.getAttribute('data-index');
-                openCanvas(links[index]);
+                const linkId = this.getAttribute('data-link-id');
+                openCanvas(linkId);
             });
         });
     }
 
-    function openCanvas(link) {
+    function openCanvas(linkId) {
+        const link = links.find(l => l.id === linkId);
         const linkPrefix = link.flags.source === 'reddit' ? 'u/' : '@';
         const urlPrefix = link.flags.source === 'reddit' ? 'https://www.reddit.com/user/' : 'https://x.com/';
         const fullUrl = urlPrefix + link.link;
-
+    
         document.getElementById('link_canvas_label').textContent = `Profile of ${link.name}`;
         document.querySelector('.offcanvas-body').innerHTML = `
             <div class="card mb-3">
                 <div class="card-body">
-                    <button type="button" class="btn btn-sm btn-neutral save-button" data-link-id="${linkId}">
-                        <i class="bi bi-save2 me-1"></i>
-                        Save
-                    </button>
                     <div class="row g-0">
                         <div class="col">
                             <div class="d-flex align-items-center">
@@ -591,101 +591,14 @@ document.addEventListener('DOMContentLoaded', () => {
                     </div>
                 </div>
             </div>
-            <div class="card mb-3">
-                <div class="card-body">
-                    <div class="row justify-content-between align-items-center">
-                        <div class="col-4">
-                            <span class="d-block h6 text-heading mb-0">${link.search.sexuality}</span>
-                            <span class="d-block text-sm text-muted">Sexuality</span>
-                        </div>
-                        <div class="col-4">
-                            <span class="d-block h6 text-heading mb-0">${link.search.body}</span>
-                            <span class="d-block text-sm text-muted">Body</span>
-                        </div>
-                        <div class="col-4">
-                            <span class="d-block h6 text-heading mb-0">${link.search.race}</span>
-                            <span class="d-block text-sm text-muted">Race</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card mb-3">
-                <div class="card-body">
-                    <span class="d-block h6 text-heading mb-0">Kinks</span>
-                    <span class="d-block text-sm text-muted">${link.search.kinks.join(', ')}</span>
-                    <hr class="my-3">
-                    <span class="d-block h6 text-heading mb-0">Tags</span>
-                    <span class="d-block text-sm text-muted">${link.search.tags.join(', ')}</span>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-body">
-                    <div class="d-flex align-items-center">
-                        <div>
-                            <span class="d-block h6 text-heading mb-0">Give ${link.name} a rating</span>
-                            <span class="d-block text-sm text-muted">0 Votes | Your Vote <b>5</b></span>
-                        </div>
-                        <button class="btn btn-sm btn-neutral ms-auto">Information</button>
-                    </div>
-                    <hr class="my-3">
-                    <!--
-                    <div class="rating">
-                        <input type="radio" id="star5" name="rate" value="5" />
-                        <label title="Excellent!" for="star5">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
-                            </svg>
-                        </label>
-                        <input value="4" name="rate" id="star4" type="radio" />
-                        <label title="Great!" for="star4">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
-                            </svg>
-                        </label>
-                        <input value="3" name="rate" id="star3" type="radio" />
-                        <label title="Good" for="star3">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
-                            </svg>
-                        </label>
-                        <input value="2" name="rate" id="star2" type="radio" />
-                        <label title="Okay" for="star2">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
-                            </svg>
-                        </label>
-                        <input value="1" name="rate" id="star1" type="radio" />
-                        <label title="Bad" for="star1">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
-                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path>
-                            </svg>
-                        </label>
-                    </div>
-                    -->
-                    <div class="rating">
-                        <input type="radio" id="star5" name="rate" value="5" />
-                        <label title="Excellent!" for="star5">★</label>
-
-                        <input value="4" name="rate" id="star4" type="radio" />
-                        <label title="Great!" for="star4">★</label>
-
-                        <input value="3" name="rate" id="star3" type="radio" />
-                        <label title="Good" for="star3">★</label>
-
-                        <input value="2" name="rate" id="star2" type="radio" />
-                        <label title="Okay" for="star2">★</label>
-
-                        <input value="1" name="rate" id="star1" type="radio" />
-                        <label title="Bad" for="star1">★</label>
-                    </div>
-                </div>
-            </div>
+            <!-- Other content for the canvas -->
         `;
-
+    
+        document.getElementById('link_canvas').setAttribute('data-link-id', linkId);
+        displayRating(linkId);
+        displayOpenCount(linkId);
         const linkCanvas = new bootstrap.Offcanvas(document.getElementById('link_canvas'));
         linkCanvas.show();
-        displayRating(link.originalIndex);
-        displayOpenCount(link.originalIndex);
     }
 
     const initialLinks = links.map((link, originalIndex) => ({
